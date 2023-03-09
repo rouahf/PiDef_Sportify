c68d8f40a41d1c46423a3d894c9b43648801512e
MaherKaroui <maher.karoui@esprit.tn>
2023-03-09 04:58:02 +0100

    mobile cours

---
+++ src/com/mycompany/gui/ActivateForm.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2016, Codename One
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ */
+
+package com.mycompany.gui;
+
+import com.codename1.components.FloatingHint;
+import com.codename1.components.SpanLabel;
+import com.codename1.ui.Button;
+import com.codename1.ui.Container;
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.TextField;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.util.Resources;
+import com.mycompany.services.coursServices;
+import com.sun.mail.smtp.SMTPTransport;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Account activation UI
+ *
+ * @author Shai Almog
+ */
+public class ActivateForm extends BaseForm {
+
+    public ActivateForm(Resources res) {
+        super(new BorderLayout());
+        Toolbar tb = new Toolbar(true);
+        setToolbar(tb);
+        tb.setUIID("Container");
+        getTitleArea().setUIID("Container");
+        Form previous = Display.getInstance().getCurrent();
+        tb.setBackCommand("", e -> previous.showBack());
+        setUIID("Activate");
+        
+        add(BorderLayout.NORTH, 
+                BoxLayout.encloseY(
+                        new Label(res.getImage("smily.png"), "LogoLabel"),
+                        new Label("Awsome Thanks!", "LogoLabel")
+                )
+        );
+        
+        TextField code = new TextField("", "Enter Code", 20, TextField.PASSWORD);
+        code.setSingleLineTextArea(false);
+        
+        Button signUp = new Button("Sign Up");
+        Button resend = new Button("Resend");
+        resend.setUIID("CenterLink");
+        Label alreadHaveAnAccount = new Label("Already have an account?");
+        Button signIn = new Button("Sign In");
+        signIn.addActionListener(e -> previous.showBack());
+        signIn.setUIID("CenterLink");
+        
+        Container content = BoxLayout.encloseY(
+                new FloatingHint(code),
+                createLineSeparator(),
+                new SpanLabel("We've sent the confirmation code to your email. Please check your inbox", "CenterLabel"),
+                resend,
+                signUp,
+                FlowLayout.encloseCenter(alreadHaveAnAccount, signIn)
+        );
+        content.setScrollableY(true);
+        add(BorderLayout.SOUTH, content);
+        signUp.requestFocus();
+        signUp.addActionListener(e -> new NewsfeedForm(res).show());
+    }
+    //sendMail
+    
+   public void sendMail(Resources res) {
+        try {
+            
+            Properties props = new Properties();
+                props.put("mail.transport.protocol", "smtp"); //SMTP protocol
+		props.put("mail.smtps.host", "smtp.gmail.com"); //SMTP Host
+		props.put("mail.smtps.auth", "true"); //enable authentication
+             
+            Session session = Session.getInstance(props,null); // aleh 9rahach 5ater mazlna masabinach javax.mail .jar
+            
+            
+            MimeMessage msg = new MimeMessage(session);
+            
+            msg.setFrom(new InternetAddress("Reintialisation mot de passe <MonEmail@domaine.com>"));
+            msg.setRecipients(Message.RecipientType.TO, "maher.karoui@esprit.tn");//mail ily bech tab
+            msg.setSubject("Application nom  : Confirmation du ");
+            msg.setSentDate(new Date(System.currentTimeMillis()));
+            
+            //String mp = coursServices.getInstance().getPasswordByEmail(email.getText().toString(), res);//mp taw narj3lo
+           String txt = "Bienvenue sur AppNom : Tapez ce mot de passe :  dans le champs requis et appuiez sur confirmer";
+           
+           
+           msg.setText(txt);
+           
+          SMTPTransport  st = (SMTPTransport)session.getTransport("smtps") ;
+            
+          st.connect("smtp.gmail.com",465,"maher.karoui@esprit.tn","201JMT4217");
+           
+          st.sendMessage(msg, msg.getAllRecipients());
+            
+          System.out.println("server response : "+st.getLastServerResponse());
+          
+        }catch(Exception e ) {
+            e.printStackTrace();
+        }
+    }
+}
